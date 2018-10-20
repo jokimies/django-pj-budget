fbc9540f0e8a54eb372f2a7016d01cb03f7f6cdf
Petri Jokimies <petri.jokimies@iki.fi>
2018-10-20 14:59:48 +0300

    Remove deprecation warning on urls
    Fixed
    	RemovedInDjango110Warning: django.conf.urls.patterns() is
    	deprecated and will be removed in Django 1.10. Update your
    	urlpatterns to be a list of django.conf.urls.url() instances
    	instead.
    warning.

--- budget/transactions/urls.py
+++ budget/transactions/urls.py
@@ -1,8 +1,15 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
+from budget.transactions import views as budget_transactions_views
 
-urlpatterns = patterns('budget.transactions.views',
-    url(r'^$', 'transaction_list', name='budget_transaction_list'),
-    url(r'^add/$', 'transaction_add', name='budget_transaction_add'),
-    url(r'^edit/(?P<transaction_id>\d+)/$', 'transaction_edit', name='budget_transaction_edit'),
-    url(r'^delete/(?P<transaction_id>\d+)/$', 'transaction_delete', name='budget_transaction_delete'),
-)
+urlpatterns = [
+    url(r'^$', budget_transactions_views.transaction_list,
+        name='budget_transaction_list'),
+    url(r'^add/$', budget_transactions_views.transaction_add,
+        name='budget_transaction_add'),
+    url(r'^edit/(?P<transaction_id>\d+)/$',
+        budget_transactions_views.transaction_edit,
+        name='budget_transaction_edit'),
+    url(r'^delete/(?P<transaction_id>\d+)/$',
+        budget_transactions_views.transaction_delete,
+        name='budget_transaction_delete'),
+]
